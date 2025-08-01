517c26db48cd7b38202294b85e45c29dc4bab059
ulfjoh <ulf.k.johansson@gmail.com>
2025-08-01 12:52:30 -0400

    Added servcice accounts back!!

--- serviceaccounts.tf
+++ serviceaccounts.tf
@@ -1,13 +1,13 @@
-## SERVICE ACCOUNTS
-#module "service_accounts" {
-  #source        = "terraform-google-modules/service-accounts/google"
-  #project_id    = var.project-id
-  #prefix        = "viewer-sa"
-  #names         = ["dev", "qa","stage","prod"]
-  #project_roles = [
-  #  "${var.project-id}=>roles/viewer",
-  #  "${var.project-id}=>roles/storage.objectViewer",
-  #]
-  #grant_billing_role = true
-  #org_id = var.org_id
-#}
+# SERVICE ACCOUNTS
+module "service_accounts" {
+  source        = "terraform-google-modules/service-accounts/google"
+  project_id    = var.project-id
+  prefix        = "viewer-sa"
+  names         = ["dev", "qa","stage","prod"]
+  project_roles = [
+    "${var.project-id}=>roles/viewer",
+    "${var.project-id}=>roles/storage.objectViewer",
+  ]
+  grant_billing_role = true
+  org_id = var.org_id
+  }
